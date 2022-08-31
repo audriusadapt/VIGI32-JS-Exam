57bd368a4ded9f5e75f6ff95b57da26ceb321490
audriusadapt <audrius@adaptagency.com>
2022-08-31 18:35:44 +0300

    init

--- Task 4/script.js
+++ Task 4/script.js
@@ -8,5 +8,29 @@ nurodomas gamintojas ir jo pagaminti modeliai.
 Pastaba: Sukurta kortelė, kurioje yra informacija apie automobilį (brand), turi 
 turėti bent minimalų stilių ir būti responsive;
 -------------------------------------------------------------------------- */
-
 const ENDPOINT = 'cars.json';
+
+$.getJSON("test.json", function(json) {
+    console.log(json); // this will show the info it in firebug console
+});
+
+console.log(mydata);
+const output = document.getElementById("output");
+
+const addCard = (car) => {
+    const card = document.createElement("div");
+    const brand = document.createElement("h4");
+    const models = document.createElement("ul");
+    const model = document.createElement("li");
+
+    card.classList.add("card");
+
+    output.append(card);
+    card.append(brand, moodels);
+
+    car.models.forEach(item => {
+        models.append(model);
+        model.textContent = item;
+    });
+
+}
